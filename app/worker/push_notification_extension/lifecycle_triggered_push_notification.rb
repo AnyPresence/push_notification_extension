c7b8c46d6d19da0910b6d741bfc8524af979ae85
Jeff Bozek <jeff.bozek@gmail.com>
2012-08-22 12:00:11 -0400

    Message payload handle correctly in worker.

--- app/worker/push_notification_extension/lifecycle_triggered_push_notification.rb
+++ app/worker/push_notification_extension/lifecycle_triggered_push_notification.rb
@@ -1,7 +1,7 @@
 class PushNotificationExtension::LifecycleTriggeredPushNotification
   @queue = :push_notification
 
-  def self.perform(channel_identifier, badge = 0, alert, message_payload = {})
+  def self.perform(channel_identifier, badge = 0, alert, message_payload)
     channel = ::PushNotificationExtension::Channel.where(name: channel_identifier).first || ::PushNotificationExtension::Channel.create(name: channel_identifier)
     channel.publish(badge, alert, message_payload)
   end
