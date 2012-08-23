97162d69be1f391e4b974f949b36cd6e6bb2163f
Jeff Bozek <jeff.bozek@gmail.com>
2012-08-23 09:32:06 -0400

    Checking for prod versus dev cert types and raising an error if nothing is configured.

--- lib/ap/push_notification_extension/push_notification.rb
+++ lib/ap/push_notification_extension/push_notification.rb
@@ -10,14 +10,24 @@ module AP
         @@config[:gcm_api_key] = config[:gcm_api_key]
         @@config[:apple_cert] = config[:apple_cert]
         @@config[:apple_cert_password] = config[:apple_cert_password]
-        
-        # This is for production apps/certs only.
-        # APNS.host = 'gateway.push.apple.com' 
 
-        APNS.pem  = "#{Rails.root}/#{::AP::PushNotificationExtension::PushNotification.config[:apple_cert]}"
-        APNS.pass = ::AP::PushNotificationExtension::PushNotification.config[:apple_cert_password] unless ::AP::PushNotificationExtension::PushNotification.config[:apple_cert_password].blank?
+        cert_valid? = false
+        if @@config[:apple_cert] && File.file?("#{Rails.root}/#{::AP::PushNotificationExtension::PushNotification.config[:apple_cert]}")
 
-        APNS.port = 2195
+          APNS.pem  = "#{Rails.root}/#{::AP::PushNotificationExtension::PushNotification.config[:apple_cert]}"
+          APNS.pass = ::AP::PushNotificationExtension::PushNotification.config[:apple_cert_password] unless ::AP::PushNotificationExtension::PushNotification.config[:apple_cert_password].blank?
+
+          pem_file = File.open("#{Rails.root}/#{::AP::PushNotificationExtension::PushNotification.config[:apple_cert]}")
+          pem_file_contents = pem_file.read
+          unless pem_file_contents.match(/Apple Development IOS Push Services/)
+            # This is for production apps/certs only.
+            APNS.host = 'gateway.push.apple.com' 
+          end
+          pem_file.close
+          APNS.port = 2195
+          cert_valid? = true
+        end
+        raise "No push services configured!" unless cert_valid? || @@config[:gcm_api_key]
       end
       def self.config
         @@config
