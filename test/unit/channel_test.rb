603fc133f1a6f11907d756eebd0ec8391d73e936
Khem Veasna <kveasna@anypresence.com>
2012-08-29 23:32:18 -0400

    Added more test.

--- test/unit/channel_test.rb
+++ test/unit/channel_test.rb
@@ -2,4 +2,14 @@ require 'test_helper'
 
 class ChannelTest < ActiveSupport::TestCase
   
+  test "should publish to GCM and APN with valid attributes" do
+    channel = FactoryGirl.build_stubbed(:channel)
+    Rails.stubs(:env => stub(production?: true)).returns(true)
+    AP::PushNotificationExtension::PushNotification.stubs(:config).returns(gcm_api_key: "some_key") 
+    GCM.any_instance.expects(:send_notification)
+    APNS.expects(:send_notifications)
+    
+    channel.publish(0, "some_alert", "some_payload")
+  end
+  
 end
