d25541dbd741767e4fa783a280a30ebbf30f852d
Khem Veasna <kveasna@anypresence.com>
2013-01-24 17:41:25 -0500

    Fixed stylesheets serving issue.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :outages
-
-  mount PushNotificationExtension::Engine => "/push_notification_extension"
+  namespace :api do
+    namespace :v1 do  
+      resources :outages
+    end
+    mount PushNotificationExtension::Engine => "/push_notification_extension"
+  end
+ 
 end
