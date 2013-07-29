699289f738a5d1722d21b89cbad8d113bdae18da
Khem Veasna <kveasna@anypresence.com>
2013-07-29 11:02:38 -0400

    Open up classes to allow testing of dummy app.

--- test/dummy/config/initializers/extensions.rb
+++ test/dummy/config/initializers/extensions.rb
@@ -3,3 +3,13 @@ class ActionController::Base
   def authenticate_admin!
   end
 end
+
+class ActionDispatch::Routing::RoutesProxy
+  def admin_root_path
+    ""
+  end
+  
+  def admin_extensions_path
+    ""
+  end
+end
