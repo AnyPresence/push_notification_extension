61536f812c4fa4826a987a5ed585eb118d95ef61
David Benko <dbenko@anypresence.com>
2014-01-23 14:29:52 -0500

    Revert

--- lib/generators/push_generator.rb
+++
@@ -1,6 +0,0 @@
-class PushNotificationExtensionRailtie < Rails::Generators::Base
-  source_root(File.expand_path(File.dirname(__FILE__)))
-  def copy_initializer
-    copy_file 'push_configurations.rb', 'config/initializers/push_configurations.rb'
-  end
-end
