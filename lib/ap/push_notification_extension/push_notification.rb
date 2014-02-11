d90c6fc5d450a41ba29298fad86ae78e03dfd5a4
Khem Veasna <kveasna@anypresence.com>
2014-02-11 17:29:33 -0500

    Read apple cert file location from environment variable
    * cleanup whitespace

--- lib/ap/push_notification_extension/push_notification.rb
+++ lib/ap/push_notification_extension/push_notification.rb
@@ -4,21 +4,13 @@ module AP
       @@config = Hash.new
       def self.config_account(config={})
         config = HashWithIndifferentAccess.new(config)
-        
-        if ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY'].blank?
-          @@config[:gcm_api_key] = config[:gcm_api_key]
-        else
-          @@config[:gcm_api_key] = ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY']
-        end
-            
-        @@config[:apple_cert] = config[:apple_cert]
-        
-        if ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD'].blank?
-          @@config[:apple_cert_password] = config[:apple_cert_password]
-        else
-          @@config[:apple_cert_password] = ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD']
-        end
-        
+
+        @@config[:gcm_api_key] = ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY'].blank? ? config[:gcm_api_key] : ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY']
+
+        @@config[:apple_cert] = ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT'].blank? ? config[:apple_cert] : ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT']
+
+        @@config[:apple_cert_password] = ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD'].blank? ? config[:apple_cert_password] :  ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD']
+
         cert_valid = false
         if @@config[:apple_cert] && File.file?("#{Rails.root}/#{::AP::PushNotificationExtension::PushNotification.config[:apple_cert]}")
 
@@ -29,7 +21,7 @@ module AP
           pem_file_contents = pem_file.read
           unless pem_file_contents.match(/Apple Development IOS Push Services/)
             # This is for production apps/certs only.
-            APNS.host = 'gateway.push.apple.com' 
+            APNS.host = 'gateway.push.apple.com'
           end
           pem_file.close
           APNS.port = 2195
@@ -37,15 +29,15 @@ module AP
         end
         raise "No push services configured!" unless cert_valid || @@config[:gcm_api_key]
       end
-      
+
       def self.config
         @@config
       end
-      
+
       def self.json_config
         @@json ||= ActiveSupport::JSON.decode(File.read("#{File.dirname(__FILE__)}/../../../manifest.json"))
       end
-      
+
       def execute_push_notification(object, options = {})
         options = HashWithIndifferentAccess.new(options)
         channel = ::PushNotificationExtension::Channel.where(name: options[:channel]).first || ::PushNotificationExtension::Channel.create(name: options[:channel])
--- test/unit/push_notification_test.rb
+++ test/unit/push_notification_test.rb
@@ -1,24 +1,42 @@
 require 'test_helper'
 
 class PushNotificationTest < ActiveSupport::TestCase
-  
-  test "should raise exception with empty config" do
-    assert_raise(RuntimeError) { AP::PushNotificationExtension::PushNotification.config_account({}) }
+
+  test "should know how to setup from environment variables" do
+    ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY'] = "123"
+    ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT'] = "345"
+    ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD'] = "password"
+
+    AP::PushNotificationExtension::PushNotification.config_account
+
+    assert_equal AP::PushNotificationExtension::PushNotification.config[:gcm_api_key], "123"
+    assert_equal AP::PushNotificationExtension::PushNotification.config[:apple_cert], "345"
+    assert_equal AP::PushNotificationExtension::PushNotification.config[:apple_cert_password], "password"
   end
-  
+
+  test "should raise error when there's no configuration" do
+    ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY'] = nil
+    ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT'] = nil
+    ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT_PASSWORD'] = nil
+
+    assert_raise(RuntimeError) { AP::PushNotificationExtension::PushNotification.config_account }
+  end
+
   test "should setup APN with valid attributes" do
     File.stubs(:file?).returns(true)
     file = stub(read: "something", close: "yay")
     File.stubs(:open).returns(file)
+    ENV['AP_PUSH_NOTIFICATIONS_APPLE_CERT'] = nil
     AP::PushNotificationExtension::PushNotification.config_account(apple_cert: "tmp/cert")
-    
+
     assert_equal "tmp/cert", AP::PushNotificationExtension::PushNotification.config[:apple_cert]
     assert_equal "#{Rails.root}/tmp/cert", APNS.pem
   end
-  
-  test "should setup GCM with valid attributes" do 
+
+  test "should setup GCM with valid attributes" do
+    ENV['AP_PUSH_NOTIFICATIONS_GCM_API_KEY'] = nil
     AP::PushNotificationExtension::PushNotification.config_account(gcm_api_key: "some_key")
     assert_equal "some_key", AP::PushNotificationExtension::PushNotification.config[:gcm_api_key]
   end
-  
+
 end
